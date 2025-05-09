941fcd726cc5a2a3c9c14b83a11a2a8f780d58bd
safwan-ms <safwan.anasms@gmail.com>
2025-05-09 11:52:24 +0530

    feat: Create register page for Auth.js v5 integration

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("MongoDB connected Successfully ✅");
+  } catch (error: unknown) {
+    const e = error as Error;
+    console.log("MongoDB connection failed ❎ ", e.message);
+  }
+};
